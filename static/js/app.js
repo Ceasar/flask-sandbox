c8021a7420bf0baf4426334320a75d36fd87e977
Ceasar Bautista <cbautista2010@gmail.com>
2014-07-05 11:12:09 -0700

    get ajax button working

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,19 @@
-$( "form" ).on( "submit", function( event ) {
-  console.log( $( this ).serialize() );
+$("form").on("submit", function(event) {
+  console.log($(this).serialize());
+});
+
+$("button").click(function(event) {
+    var $form = $("form");
+    var data = {
+        name: $form.find("input[name=name]").val(),
+        animals: $form.find("select").val(),
+    };
+    console.log(data);
+    $.ajax({
+        url: "/",
+        type: "POST",
+        data: data,
+        dataType: "json",
+        traditional: true
+    });
 });
