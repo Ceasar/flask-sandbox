0c3c25ccedeb1acf06f353c99d16b2e532cccee8
Ceasar Bautista <cbautista2010@gmail.com>
2014-07-17 09:49:51 -0700

    Demonstrate how add_url_rule can be bypassed

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 
-env: 
-	virtualenv env
 
 run:
 	. env/bin/activate; pip install --requirement requirements.txt
 	. env/bin/activate; python app.py
+
+env: 
+	virtualenv env
--- app.py
+++ app.py
@@ -11,5 +11,18 @@ def index():
             print x
     return render_template("index.html")
 
+
+# The following demonstrates how ``add_url_rule`` (and ``route``) can be
+# bypassed; the following are (essentially) equivalent.
+
+app.route("/a", endpoint="a", methods=['GET'])(lambda: "a")
+
+app.add_url_rule("/b", endpoint="b", methods=['GET'])
+app.endpoint("b")(lambda: "b")
+
+app.url_map.add(app.url_rule_class("/c", endpoint="c", methods=['GET']))
+app.view_functions["c"] = lambda: "c"
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=4000)
